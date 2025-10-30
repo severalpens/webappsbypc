4fb9798abe22615f15afe20cdf580fb0352beefb
Paul Collins <paul.collins@severalpens.com>
2025-10-30 13:58:04 +1100

    Add RaceTimesAuthWrapper

---
+++ app/raceTimes/RaceTimesAuthWrapper.tsx
@@ -0,0 +1,28 @@
+// import { Authenticator } from '@aws-amplify/ui-react'; // Import Auth module
+// import '@aws-amplify/ui-react/styles.css';
+// import RaceTimes from "./RaceTimes";
+
+
+// function RaceTimesAuthWrapper() {
+//   // const [usr, setUsr] = useState<string>('');
+//   // const [loginAttempts, setLoginAttempts] = useState<number>(0);
+
+//   return (
+//     <Authenticator>
+//       {({ signOut, user }) => {
+//         return (
+//         <div className="mt-5">
+//         {user && <RaceTimes user={user} />}
+//         <div className="flex justify-end">
+//           <button onClick={signOut} className="mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  w-48">
+//             Sign out
+//           </button>
+//           </div>
+//         </div>
+//       )}}
+//     </Authenticator>
+//   );
+// }
+
+// export default RaceTimesAuthWrapper;
+
